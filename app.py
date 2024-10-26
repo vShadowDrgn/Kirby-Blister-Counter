ab191f8c31cd33259bc4fdabec6177bb34a2d11d
vShadowDrgn <horzen1999@gmail.com>
2024-10-26 22:56:45 +0200

    counter und button

--- app.py
+++ app.py
@@ -3,13 +3,8 @@ from flask import Flask, render_template, request, redirect, url_for, jsonify
 from database_manager import Dao # type: ignore
 #from hardware import ir_loop # type: ignore
 
-counter = 0
 app = Flask(__name__)
 database = Dao("database.db")
-
-def increase_counter():
-    global counter
-    counter += 1
 
 @app.route('/')
 def index():
@@ -19,7 +14,12 @@ def index():
 def statistik():
     return render_template("statistik.html")
 
+@app.route('/increase_counter')
+def increase_counter():
+    database.increase_counter()
+    return jsonify({"success": True, "message": "Erfolgreich gezählt"})
+
 if __name__ == "__main__":
     #t = Thread(target=ir_loop, args=(database,))
     #t.start()
-    app.run(host="0.0.0.0", debug=True, use_reloader=False)
+    app.run(host="0.0.0.0", debug=True) #, use_reloader=False)
--- database_manager.py
+++ database_manager.py
@@ -41,5 +41,31 @@ class Dao:
 
         except sqlite3.Error as err:
             sql_error_handler(err,traceback.format_exc())
+    
+    def increase_counter(self):
+        try:
+            conn, cursor = self.get_db_connection()
+
+            sql = """INSERT INTO counter (action) VALUES ('insert')"""       
+            cursor.execute(sql)
+            conn.commit()
+            conn.close()
+
+        except sqlite3.Error as err:
+            sql_error_handler(err,traceback.format_exc())
+
+    def reset_counter(self):
+        try:
+            conn, cursor = self.get_db_connection()    
+
+            sql = """INSERT INTO counter (action) VALUES ('reset')"""       
+            cursor.execute(sql)
+            conn.commit()
+            conn.close()
+
+        except sqlite3.Error as err:
+            sql_error_handler(err,traceback.format_exc())
+            
+    
 
 db = Dao("database.db")
